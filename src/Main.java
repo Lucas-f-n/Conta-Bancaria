44a90036b12181f01313ab9af12ceb1f489ef94e
Lucas Fernando <lucas.f.n.silv@gmail.com>
2025-10-14 11:50:45 -0300

    Primeiro commit do projeto Conta Bancaria

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] arcs){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("""
+                **************************************
+                Dados iniciais do cliente:
+                
+                Nome:                Lucas
+                Tipo Conta:          Corrente
+                Saldo inicial:       R$ 2500,00
+                
+                **************************************
+                """);
+    }
+}
