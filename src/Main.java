7712ad913aaaa24777280cab56ef4299e4b60c48
Lucas Fernando <lucas.f.n.silv@gmail.com>
2025-10-15 11:59:54 -0300

    Criando loop de menu, adicionado saldo e receber valor

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,33 @@ public class Main {
                 
                 **************************************
                 """);
+        int menu;
+        double saldo = 2500.00;
+        double receber;
+        do{
+         System.out.println("""
+                 Operações:
+                 
+                 1- Consultar Saldos
+                 2- Receber Valor
+                 3- Transferir Valor
+                 4- Sair
+                 
+                 Digite a opção desejada:
+                 """);
+         menu = scanner.nextInt();
+         switch (menu){
+             case 1:
+                 System.out.printf("O saldo atual e R$%.2f\n",saldo);
+                 break;
+             case 2:
+                 System.out.println("Informe o valor a receber:");
+                 receber = scanner.nextDouble();
+                 saldo += receber;
+                 break;
+         }
+
+        }while ( menu != 4);
+
     }
 }
